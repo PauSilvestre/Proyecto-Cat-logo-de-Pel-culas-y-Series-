aa157685dcc4ad1ed1055af35311b782c44b1b9c
PauSilvestre <pausilarn@alu.edu.gva.es>
2025-10-17 13:47:03 +0200

    Menu, estructura del pryecto, clases creadas y definidas, main hecho, solo falta hacer las funciones de las clases, definirlas y hacerlas

--- movierepo2/src/clases/Pelicula.java
+++ movierepo2/src/clases/Pelicula.java
@@ -1,5 +1,17 @@
 package clases;
 
 public class Pelicula {
+    private String titulo;
+    private String director;
+    private int año;
+    private String genero;
+    private int valoracion;
 
+    public Pelicula(String titulo, String director, int año, String genero, int valoracion) {
+        this.titulo = titulo;
+        this.director = director;
+        this.año = año;
+        this.genero = genero;
+        this.valoracion = valoracion;
+    }
 }

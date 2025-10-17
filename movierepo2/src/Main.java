ae3dce4c196a4b61c26d632b26d9e0122eccf7d9
PauSilvestre <pausilarn@alu.edu.gva.es>
2025-10-17 13:41:03 +0200

    Add files via upload

---
+++ movierepo2/src/Main.java
@@ -0,0 +1,10 @@
+import clases.Pelicula;
+import clases.GestorCatalogo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ movierepo2/src/clases/GestorCatalogo.java
@@ -0,0 +1,9 @@
+package clases;
+
+import clases.Pelicula;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorCatalogo {
+
+}
